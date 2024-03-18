2a7667be7bd2703b15f495ec31844dc0969d3ab1
neerajjoshi-github <joshineeraj7037@gmail.com>
2024-03-18 19:16:19 +0530

    Updated authorization provider to remove localstorage error

--- client/components/providers/AuthorizationProvider.tsx
+++ client/components/providers/AuthorizationProvider.tsx
@@ -1,14 +1,15 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { RootState } from "@/lib/reduxStore/store";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setUser } from "@/lib/reduxStore/slices/userSlice";
 
 const AuthorizationProvider = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const user = JSON.parse(localStorage.getItem("userInfo") as string) || null;
+  const user =
+    JSON.parse(localStorage && (localStorage.getItem("userInfo") as string)) ||
+    null;
   dispatch(setUser(user));
 
   if (user) {
