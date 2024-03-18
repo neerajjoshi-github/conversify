8e70bf439a71049321ace04506970c119f9c7b28
neerajjoshi-github <joshineeraj7037@gmail.com>
2024-03-18 20:05:45 +0530

    Fixing localstorage error

--- client/components/providers/AuthorizationProvider.tsx
+++ client/components/providers/AuthorizationProvider.tsx
@@ -1,18 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/lib/reduxStore/slices/userSlice";
 
 const AuthorizationProvider = ({ children }: { children: React.ReactNode }) => {
+  const [currentUser, setCurrentUser] = useState<any | null>(null);
   const dispatch = useDispatch();
   const router = useRouter();
-  const user =
-    JSON.parse(localStorage && (localStorage.getItem("userInfo") as string)) ||
-    null;
-  dispatch(setUser(user));
 
-  if (user) {
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo") as string) || null;
+    setCurrentUser(user);
+  }, []);
+
+  dispatch(setUser(currentUser));
+
+  if (currentUser) {
     return children;
   } else {
     router.replace("/login");
