5d5652839a5615be954e65ea0680c2e16ddc4bc1
neerajjoshi-github <joshineeraj7037@gmail.com>
2024-05-02 13:57:40 +0530

    Fixed AuthorizationProvider error realted to useEffect

--- client/components/providers/AuthorizationProvider.tsx
+++ client/components/providers/AuthorizationProvider.tsx
@@ -1,29 +1,20 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/lib/reduxStore/slices/userSlice";
 
 const AuthorizationProvider = ({ children }: { children: React.ReactNode }) => {
-  const [currentUser, setCurrentUser] = useState<any | null>(null);
+  const user = JSON.parse(localStorage.getItem("userInfo") as string) || null;
   const dispatch = useDispatch();
   const router = useRouter();
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("userInfo") as string) || null;
-    setCurrentUser(user);
-  }, []);
-
-  dispatch(setUser(currentUser));
-
-  if (currentUser) {
-    return children;
+  if (!user) {
+    router.replace("/login");
   } else {
-    useEffect(() => {
-      router.replace("/login");
-    }, []);
-    return null;
+    dispatch(setUser(user));
   }
+
+  return user ? <>{children}</> : null;
 };
 
 export default AuthorizationProvider;
